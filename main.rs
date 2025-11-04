94c2ac41de0b44cd0ed49c998a5af5761a6c12b2
Torrin <38574537+torrinjbrown@users.noreply.github.com>
2025-11-04 06:10:19 -0500

    Added bullets for the object
    Going to use this to take down the random objects that are moving

--- main.rs
+++ main.rs
@@ -1,18 +1,34 @@
 //This is the package for gaming library in rust
 use macroquad::prelude::*;
 
+//Creating the outline for each variable that will be added to the game 
 struct Shape {
         size: f32,
         speed: f32,
         x: f32,
         y: f32,
+        collided: bool,
+    }
+
+//Creating the shape for the rectangle and specifying size
+impl Shape {
+        fn collides_with(&self, other: &Self) -> bool {
+            self.rect().overlaps(&other.rect())
+        }
+        fn rect(&self) -> Rect {
+            Rect {
+                x: self.x - self.size / 2.0,
+                y: self.y - self.size / 2.0,
+                w: self.size,
+                h: self.size,
+            }
+        }
     }
 //Calling the library through # and the folder
 #[macroquad::main("My game")]
 
 //Creating a async function to make a continous loop for the background 
 async fn main() {
-    
     //Sticking the movement speed of the game
     const MOVEMENT_SPEED: f32 = 200.0;
     
@@ -20,16 +36,22 @@ async fn main() {
     rand::srand(miniquad::date::now() as u64);
     let mut squares = vec![];
     
+    //Creating the bullets for the game 
+    let mut bullets: Vec<Shape> = vec![];
+
     let mut circle = Shape {
         size: 32.0,
         speed: MOVEMENT_SPEED,
         x: screen_width() / 2.0,
         y: screen_height() / 2.0,
+        collided: false,
     };
-    
+    let mut gameover = false;
+
     loop {
         clear_background(DARKPURPLE);
         
+       if !gameover {
         //Capturing the time for the pixel
         let delta_time = get_frame_time();
 
@@ -46,7 +68,16 @@ async fn main() {
         if is_key_down(KeyCode::Up) {
             circle.y -= MOVEMENT_SPEED * delta_time;
         }
-
+        if is_key_pressed(KeyCode::Space) {
+            bullets.push(Shape {
+                x: circle.x,
+                y: circle.y,
+                speed: circle.speed * 2.0,
+                size: 5.0,
+                collided: false,
+            });
+        }
+    
         //Clamp X and Y to be within the screen
         circle.x = clamp(circle.x, 0.0, screen_width());
         circle.y = clamp(circle.y, 0.0, screen_height());
@@ -59,18 +90,55 @@ async fn main() {
                 speed: rand::gen_range(50.0, 150.0),
                 x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
                 y: -size,
+                collided: false,
             });
         }
         //Move Squares 
         for square in &mut squares {
             square.y += square.speed * delta_time;
         }
-
+        for bullet in &mut bullets {
+            bullet.y -= bullet.speed * delta_time;
+        }
+    }
         // Remove squares below bottom of screen
         squares.retain(|square| square.y < screen_height() + square.size);
+        bullets.retain(|bullet| bullet.y > 0.0 - bullet.size / 2.0);
+
+
+        //Remove collided shapes
+        squares.retain(|square| !square.collided);
+        bullets.retain(|bullet| !bullet.collided);
+
+
+
+        if squares.iter().any(|square| circle.collides_with(square)){
+            gameover = true;
+        }
+
+        for square in squares.iter_mut() {
+            for bullet in bullets.iter_mut() {
+                if bullet.collides_with(square) {
+                    bullet.collided = true;
+                    square.collided = true;
+                }
+            }
+        }
+
+        if gameover && is_key_pressed(KeyCode::Space) {
+            squares.clear();
+            circle.x = screen_width() / 2.0;
+            circle.y = screen_height() / 2.0;
+            gameover = false;
+        }
 
         //Drawing everything
-        draw_circle(circle.x, circle.y, circle.size / 2.0, YELLOW);
+
+        for bullet in &bullets {
+            draw_circle(bullet.x, bullet.y, bullet.size / 1.0, RED);
+        }
+
+        draw_circle(circle.x, circle.y, circle.size / 3.0, YELLOW);
         for square in &squares {
             draw_rectangle(
                 square.x - square.size / 2.0,
@@ -78,6 +146,17 @@ async fn main() {
                 square.size,
                 square.size,
                 GREEN,
+            );
+        }
+        if gameover {
+            let text = "Try Again";
+            let text_dimensions = measure_text(text, None, 50, 1.0);
+            draw_text (
+                text,
+                screen_width() / 2.0 -text_dimensions.width / 2.0,
+                screen_height() / 2.0,
+                50.0,
+                RED,
             );
         }
         next_frame().await    
